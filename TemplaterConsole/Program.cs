0ddb72f69118f18ca7808c554885a21451726aa6
Ivan G <girs874@gmail.com>
2024-02-03 01:50:14 +0400

    Adds exception handling

--- TemplaterConsole/Program.cs
+++ TemplaterConsole/Program.cs
@@ -1,19 +1,60 @@
 ﻿using TemplaterLibrary;
 
-string message = args.Length != 3 ? 
-    "Invalid command line arguments.\n" +
-    "Please provide the 3 mandatory arguments:\n" +
-    "[templateFilePath] [dataFilePath] [outputFilePath]" : 
-    string.Empty;
-if (message.Length > 0)
+if (args.Length != 3)
 {
-    Console.WriteLine(message);
+    Console.WriteLine("Invalid command line arguments.\n" +
+    "Please provide the 3 mandatory arguments:\n" +
+    "[templateFilePath] [dataFilePath] [outputFilePath]");
     return;
 }
 
-string jsonData = File.ReadAllText(args[1]);
-string template = File.ReadAllText(args[0]);
+if (!(File.Exists(args[0]) && File.Exists(args[1])))
+{
+    Console.WriteLine("Invalid command line arguments.\n" +
+    "Please check if template and data files " +
+    "present in the specified locations");
+    return;
+}
+
+string template;
+string jsonData;
+try
+{
+    template = File.ReadAllText(args[0]);
+    jsonData = File.ReadAllText(args[1]);
+}
+catch (IOException)
+{
+    Console.WriteLine("There was an error reading a file.");
+    return;
+}
+
+if (string.IsNullOrWhiteSpace(template) || string.IsNullOrWhiteSpace(jsonData))
+{
+    Console.WriteLine("Files provided are not valid.");
+    return;
+}
+
 Templater templater = new();
-string result = templater.CreateHtml(template, jsonData);
-File.WriteAllText(args[2], result);
+string result;
+try
+{
+    result = templater.CreateHtml(template, jsonData);
+}
+catch (Newtonsoft.Json.JsonReaderException)
+{
+    Console.WriteLine("Failed to retrieve data from JSON file.");
+    return;
+}
+
+try
+{
+    File.WriteAllText(args[2], result);
+}
+catch (IOException)
+{
+    Console.WriteLine("There was an error writing results to a file.");
+    return;
+}
+
 Console.WriteLine(result);
